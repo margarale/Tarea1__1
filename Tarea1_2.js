798e6b1066d40b42683e856526847bbe5f92da6f
margarale <65833089+margarale@users.noreply.github.com>
2020-06-19 22:34:39 -0500

    Create Tarea1_2.js

---
+++ Tarea1_2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+  background(153);
+
+fill('red'); // Use 'redValue' in new fill
+circle(100, 100, 180); // Draw right rectangle
+
+fill(255);
+arc(100, 100, 135, 135, radians(200), radians(370), CHORD);
+  
+fill(255); // Use color variable 'c' as fill color
+arc(100, 100, 135, 135, radians(15), radians(195), CHORD);
+  
+   
+  
+  
+}
